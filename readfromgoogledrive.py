b9d60ef1bcfabecc9ffcc173f203e047db301ffb
Muhammad Suleman <leodeveloper@gmail.com>
2024-06-27 08:27:40 +0400

    port 8080

--- readfromgoogledrive.py
+++ readfromgoogledrive.py
@@ -52,7 +52,7 @@ def getfilefromdrive(fileId):
               }
       flow =InstalledAppFlow.from_client_config(config,SCOPES)
       #flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
-      creds = flow.run_local_server(host=redirecturi, port=5000)
+      creds = flow.run_local_server(port=8080)
     # Save the credentials for the next run
     with open("token.json", "w") as token:
       token.write(creds.to_json())
