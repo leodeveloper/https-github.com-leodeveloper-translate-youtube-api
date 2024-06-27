626f0c8a8625a66f1c74b2ff77d9b2a479ca2f4c
Muhammad Suleman <leodeveloper@gmail.com>
2024-06-27 08:34:11 +0400

    add host

--- readfromgoogledrive.py
+++ readfromgoogledrive.py
@@ -1,6 +1,6 @@
 import os.path
 import io
-
+import webbrowser
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -52,7 +52,7 @@ def getfilefromdrive(fileId):
               }
       flow =InstalledAppFlow.from_client_config(config,SCOPES)
       #flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
-      creds = flow.run_local_server(port=8080)
+      creds = flow.run_local_server(host=redirecturi,port=8080)
     # Save the credentials for the next run
     with open("token.json", "w") as token:
       token.write(creds.to_json())
